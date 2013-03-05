6ac0b39f1224d82a66f89d3eb5f6a1479d1513aa
soarcn <soarcn@gmail.com>
2013-03-05 14:36:54 +0800

    initial version

--- README.md
+++ README.md
@@ -2,3 +2,35 @@ TaskFragment
 ============
 
 TaskFragment is designed to provide a container for complex &amp; reusable functions in Android platform.
+
+
+Why you need it
+=============
+
+In daily development work, you will find some functions would be reused in different components/projects, but hard to package them to a class/method. for example, take/pick photos, Authorization for SNS etc.
+
+Because of Android's Design priciple, Use 3rd party Activity is very common in Android app development, but use onActivityResult interface is the only way to get the result, that's the main reason we can't create a simple reusable component in those case.
+
+TaskFragment is create for this purpose.
+
+Dependency
+=============
+
+- Android support libary r10
+
+
+How to use TaskFragment
+=======
+- It's a quite simple abstract class file, you can feel free to copy it to your project folder.
+- Inherit and implement it.
+
+
+Contribute
+=======
+
+- feel free to Fork it
+
+
+Other
+=======
+- Demo will be created soon.
---
+++ TaskFragment.java
@@ -0,0 +1,83 @@
+﻿package com.cocosw.taskfragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.View;
+
+/**
+ * TaskFragment is designed to provide a container for complex &amp; reusable
+ * functions in Android platform.
+ * 
+ * @author soar
+ * 
+ */
+public abstract class TaskFragment<V> extends Fragment {
+
+	private CallBack<V> callback;
+	// You can even use targetFragment's UI
+	protected View view;
+
+	@Override
+	public void onActivityCreated(final Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		if (getTargetFragment() != null) {
+			view = (getTargetFragment().getView());
+		}
+		work(getArguments());
+	}
+
+	protected abstract void work(final Bundle b);
+
+	protected void run(final Fragment f, final Bundle b,
+			final CallBack<V> callback) {
+		final TaskFragment<?> lasttask = TaskFragment.getTaskFragment(f
+				.getFragmentManager());
+		if (lasttask != null) {
+			f.getFragmentManager().beginTransaction().remove(lasttask).commit();
+		}
+		if (b != null) {
+			setArguments(b);
+		}
+		setCallback(callback);
+		setTargetFragment(f, 0);
+		f.getFragmentManager().beginTransaction().add(this, "_taskfragment_")
+				.commit();
+	}
+
+	public static TaskFragment<?> getTaskFragment(final FragmentManager fm) {
+		return (TaskFragment<?>) fm.findFragmentByTag("_taskfragment_");
+	}
+
+	private void setCallback(final CallBack<V> callback) {
+		this.callback = callback;
+	}
+
+	protected void run(final Fragment f) {
+		run(f, null, null);
+	}
+
+	protected CallBack<V> getCallback() {
+		if (callback == null) {
+			callback = new CallBack<V>() {
+
+				@Override
+				public void callback(final V object) {
+				}
+
+				@Override
+				public void failback(final V object, final Exception status) {
+				}
+			};
+		}
+		return callback;
+	}
+
+	public interface CallBack<T> {
+
+		public void callback(T object);
+
+		public void failback(T object, Exception status);
+	}
+
+}
